e04609d086926da20a20a4239abf2effa919de5c
Soo-C <gochos88@naver.com>
2019-06-04 16:33:10 -0400

    learned how to use conditional.

---
+++ python/hilt_class.py
@@ -0,0 +1,4 @@
+hilt_class = ["Soo Cho", "Brandon", "Ethan", "Tony", "Kristen","David"]
+for name in hilt_class:
+    if name == "Soo Cho":
+        print(name)
---
+++ python/list_of_places.py
@@ -0,0 +1,16 @@
+
+# make a new list places
+places = ["paris", "Barcelna","Lima","Busan"]
+
+
+for place in places:
+    print ("I would like to visit" + place + " ")
+
+
+
+#places = ["paris", "Barcelna","Lima","Busan"]
+#Wish = ["I would like to visit" + " " + place for place in places]
+
+#Wish = []
+#Wish.append("I would like to visit")
+#print(Wish)
