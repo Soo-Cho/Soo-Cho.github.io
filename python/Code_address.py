efc4de0e60c027047836e92de225e2d3c3231b6d
Soo-C <gochos88@naver.com>
2019-06-04 15:08:53 -0400

    changed a bunch of stuffs.

---
+++ python/Code_address.py
@@ -0,0 +1,11 @@
+my_address = {
+"name": "Soo Cho ",
+"street address": "C. Road ",
+"city": "East Lansing ",
+"state": "MI, ",
+"zip code":"48823"
+}
+
+print(my_address["name"])
+print(my_address["street address"])
+print(my_address["city"] + ", " + my_address["state"] + " " + my_address["zip code"])
---
+++ python/book_excercise.py
@@ -0,0 +1,8 @@
+total_book = 457
+word_per_page = 250
+num_of_pieces = 100
+
+each_piece = (total_book*word_per_page)/num_of_pieces
+
+
+print(each_piece)
