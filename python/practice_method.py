3c84ee4984e61dfdaaeff3fc9911735f9480e278
Soo-C <gochos88@naver.com>
2019-06-05 10:05:42 -0400

    created my own method.

---
+++ python/practice_method.py
@@ -0,0 +1,8 @@
+def wish(food):
+    print("I am craving " + food)
+
+foods = ["bubble tea","smoothie","frappuchino","shake","icecream"]
+
+for food in foods:
+    wish(food)
+#print wish(food) - does not work
