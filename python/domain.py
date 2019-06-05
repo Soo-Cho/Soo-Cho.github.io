9ad74c7666ba780288834e12db57f9d013631368
Soo-C <gochos88@naver.com>
2019-06-05 13:50:50 -0400

    made some changes

--- python/domain.py
+++ python/domain.py
@@ -6,4 +6,15 @@ URLs = []
 for page in pages:
     URLs.append(domain + page)
 
+#for page in pages:
+    # url = domain + page
+    # urls.append(url)
+# ---> same result. but this is is easier to follow. This takes memory: cause it'll remember what url is.
+# ---> might help more later when your code gets longer
+
 print(URLs)
+
+
+
+# --> to make a comprehension: it's the same thig
+# urls = [domain + page for page in pages]
